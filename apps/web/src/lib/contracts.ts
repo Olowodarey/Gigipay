553fb53a9d04d5dbe1e30606ed06b6d199314516
Olowodarey <olowodarey8@gmail.com>
2026-04-14 15:46:24 +0100

    update create

--- apps/web/src/lib/contracts.ts
+++ apps/web/src/lib/contracts.ts
@@ -6,7 +6,7 @@ import type { Address } from "viem";
  */
 export const CONTRACT_ADDRESSES: Record<number, Address> = {
   42220: (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_CELO ||
-    "0x70b92a67F391F674aFFfCE3Dd7EB3d99e1f1E9a8") as Address, // Celo
+    "0x88D7034cc9409f78F6B00D34FeA5B0941FbeC69b") as Address, // Celo
   8453: (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_BASE ||
     "0xEdc6abb2f1A25A191dAf8B648c1A3686EfFE6Dd6") as Address, // Base
 };
